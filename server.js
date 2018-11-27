7fdf6f8827a14addb4be379218bfa5551bab54f9
Hariom Soni <hariom.soni@asticsinc.com>
2018-11-27 18:53:49 +0530

    added views and other server code

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+var upload = require('multer')().single('upfile')
+// require and use "multer"...
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.post('/api/fileanalyse', upload, function(req, res){
+  if(req.file){
+    res.json({filename:req.file.originalname, size:req.file.size, type:req.file.mimetype})
+  }
+  else{
+    res.json({error:'No file found'});
+  }
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
